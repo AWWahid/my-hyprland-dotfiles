d4d3f56ff74ef16c443280d8cbf57b4b361f5673
Abrar Wahid Wasee <awahidstudent@gmail.com>
2026-09-11 22:55:38 +0600

    Moving to gnu stow configuration

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -28,8 +28,8 @@ hl.monitor({
 ---------------------
 
 -- Set programs that you use
-local terminal    = "foot"
-local fileManager = "foot -e yazi"
+local terminal    = "kitty"
+local fileManager = "kitty -e yazi"
 local menu        = "fuzzel"
 
 
@@ -42,7 +42,7 @@ local menu        = "fuzzel"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function () 
+-- hl.on("hyprland.start", function ()
 --   hl.exec_cmd(terminal)
 --   hl.exec_cmd("nm-applet")
 --   hl.exec_cmd("waybar & hyprpaper & firefox")
